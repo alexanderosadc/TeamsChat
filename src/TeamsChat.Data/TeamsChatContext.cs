079b53397ee2e8ebaa4d3d305e982a723f4eaf89
alexanderosadc <53898827+alexOSAD@users.noreply.github.com>
2021-09-11 18:23:02 +0300

    Resolved Dependency Error

--- src/TeamsChat.Data/TeamsChatContext.cs
+++ src/TeamsChat.Data/TeamsChatContext.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using TeamsChat.DataObjects;
 namespace TeamsChat.Data
 {
     public class TeamsChatContext : DbContext
     {
+        
         public TeamsChatContext(DbContextOptions<TeamsChatContext> options) : base(options) { }
+
+        public DbSet<AttachedFiles> AttachedFiles { get; set; }
 
         //public virtual DbSet<AttachedFiles> AttachedFiles { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
