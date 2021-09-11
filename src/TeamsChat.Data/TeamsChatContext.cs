3404a86615aa0a7cee523f247d71c8a31d339d43
Maxim Volosenco <maximvolosenco@gmail.com>
2021-09-11 18:18:35 +0300

    Createad Repository files

--- src/TeamsChat.Data/TeamsChatContext.cs
+++ src/TeamsChat.Data/TeamsChatContext.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace TeamsChat.Data
 {
     public class TeamsChatContext : DbContext
     {
-        public TeamsChatContext(DbContextOptions<TeamsChatContext> options) : base(options)
-        {
-        }
+        public TeamsChatContext(DbContextOptions<TeamsChatContext> options) : base(options) { }
+
+        //public virtual DbSet<AttachedFiles> AttachedFiles { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
---
+++ src/TeamsChat.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamsChat.WebApi.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        //protected static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        //protected readonly IUnitOfWork _database;
+        //protected readonly IMapper _mapper;
+
+        //public BaseController(IUnitOfWork database, IMapper mapper)
+        //{
+        //    _database = database;
+        //    _mapper = mapper;
+        //}
+    }
+}
