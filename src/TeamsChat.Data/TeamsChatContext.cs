f581ef2fca5704f01a58cd8315119493abb1cb7f
alexanderosadc <53898827+alexOSAD@users.noreply.github.com>
2021-09-11 17:49:27 +0300

    Working on Entities

---
+++ src/TeamsChat.Data/TeamsChatContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TeamsChat.Data
+{
+    public class TeamsChatContext : DbContext
+    {
+        public TeamsChatContext(DbContextOptions<TeamsChatContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
