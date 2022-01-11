e3e4108528c5f7b9e1401f2f717de9deaebc5848
maximvolosenco <maximvolosenco@gmail.com>
2022-01-11 18:40:40 +0200

    [fix] Return losed controllers

---
+++ src/TeamsChat.WebApi/Controllers/MessagesController.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TeamsChat.WebApi.DTO;
+using TeamsChat.WebApi.Common;
+using System.Threading.Tasks;
+using TeamsChat.DatabaseInterface;
+using TeamsChat.WebApi.DbCommunicators;
+using TeamsChat.TimeoutService;
+using System.Net;
+using TeamsChat.TimeoutService.Models;
+
+namespace TeamsChat.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessagesController : BaseController
+    {
+        private MessagesCommunicator _messagesCommunicator;
+        private int _timeout = 5;
+        public MessagesController(IDatabaseFactory databaseFactory, IMapper mapper, IControllerManager controllerManager) : base(databaseFactory, mapper, controllerManager)
+        {
+            _messagesCommunicator = new MessagesCommunicator(databaseFactory, mapper, controllerManager);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MessageDTO>>> GetMessages()
+        {
+            var result = await TimeoutManager.TimeoutValidator(_messagesCommunicator.GetMessages, HttpContext, _timeout);
+
+            if (result.StatusCode == HttpStatusCode.RequestTimeout)
+                return StatusCode(408);
+
+            if (result.Output.StatusCode == HttpStatusCode.NoContent)
+                return StatusCode(204);
+
+            return Ok(result.Output.Data);
+        }
+
+        [HttpGet("groupId={groupId}")]
+        public async Task<ActionResult<IEnumerable<MessageDTO>>> GetMessagesByGroupId(int groupId)
+        {
+            TimeoutParameters<int> parameters = new TimeoutParameters<int> { Container = groupId, HttpContext = HttpContext };
+            var result = await TimeoutManager.TimeoutValidator(_messagesCommunicator.GetMessagesByGroupId, parameters, _timeout);
+
+            if (result.StatusCode == HttpStatusCode.RequestTimeout)
+                return StatusCode(408);
+
+            if (result.Output.StatusCode == HttpStatusCode.NoContent)
+                return StatusCode(204);
+
+            return Ok(result.Output.Data);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MessageDTO>> PostMessage([FromBody] MessageDTO messageDTO)
+        {
+            TimeoutParameters<MessageDTO> parameters = new TimeoutParameters<MessageDTO> { Container = messageDTO, HttpContext = HttpContext };
+            var result = await TimeoutManager.TimeoutValidator(_messagesCommunicator.PostMessage, parameters, _timeout);
+
+            if (result.StatusCode == HttpStatusCode.RequestTimeout)
+                return StatusCode(408);
+
+            if (result.Output.StatusCode == HttpStatusCode.InternalServerError)
+                return StatusCode(500);
+
+            return StatusCode(201);
+        }
+    }
+}
---
+++ src/TeamsChat.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TeamsChat.WebApi.DTO;
+using TeamsChat.WebApi.Common;
+using System.Threading.Tasks;
+using TeamsChat.DatabaseInterface;
+using TeamsChat.TimeoutService.Models;
+using TeamsChat.TimeoutService;
+using TeamsChat.WebApi.DbCommunicators;
+using System.Net;
+
+namespace TeamsChat.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : BaseController
+    {
+        private UsersCommunicator _usersCommunicator;
+        private int _timeout = 5;
+
+        public UsersController(IDatabaseFactory databaseFactory, IMapper mapper, IControllerManager controllerManager) : base(databaseFactory, mapper, controllerManager)
+        {
+            _usersCommunicator = new UsersCommunicator(databaseFactory, mapper, controllerManager);
+        }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<IEnumerable<UserDTO>>> GetUserByName([FromQuery] string firstName, string lastName)
+        {
+
+            TimeoutParameters<UserDTO> parameters = new TimeoutParameters<UserDTO> { Container = new UserDTO { FirstName = firstName, LastName = lastName }, HttpContext = HttpContext };
+            var result = await TimeoutManager.TimeoutValidator(_usersCommunicator.GetUserByName, parameters, _timeout);
+
+            if (result.StatusCode == HttpStatusCode.RequestTimeout)
+                return StatusCode(408);
+
+            if (result.Output.StatusCode == HttpStatusCode.NoContent)
+                return StatusCode(204);
+
+            return Ok(result.Output.Data);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserDTO>>> LoginUser(string email, string password)
+        {
+            TimeoutParameters<UserDTO> parameters = new TimeoutParameters<UserDTO> { Container = new UserDTO { Email = email, Password = password }, HttpContext = HttpContext };
+            var result = await TimeoutManager.TimeoutValidator(_usersCommunicator.LoginUser, parameters, _timeout);
+
+            if (result.StatusCode == HttpStatusCode.RequestTimeout)
+                return StatusCode(408);
+
+            if (result.Output.StatusCode == HttpStatusCode.Forbidden)
+                return StatusCode(403);
+
+            if (result.Output.StatusCode == HttpStatusCode.NoContent)
+                return StatusCode(204);
+
+            return Ok(result.Output.Data);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserDTO>> PostUser([FromBody] UserDTO userDTO)
+        {
+            TimeoutParameters<UserDTO> parameters = new TimeoutParameters<UserDTO> { Container = userDTO, HttpContext = HttpContext };
+            var result = await TimeoutManager.TimeoutValidator(_usersCommunicator.PostUser, parameters, _timeout);
+
+            if (result.StatusCode == HttpStatusCode.RequestTimeout)
+                return StatusCode(408);
+
+            if (result.Output.StatusCode == HttpStatusCode.Forbidden)
+                return StatusCode(403);
+
+            if (result.Output.StatusCode == HttpStatusCode.InternalServerError)
+                return StatusCode(500);
+
+            return StatusCode(201);
+        }
+    }
+}
